0246b2c5180442ab58bcb96cb3c283654fcc5c39
ansh27dev <ansh255gupta@gmail.com>
2024-03-14 02:06:21 +0530

    some more pages created

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,21 +5,39 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/profile/:id", (req, res) => {
+  const userId = req.user._id;
+  const user = userModel.findOne({});
+  res.render("profile");
+});
+
+router.get("/listing/:city", (req, res) => {
+  const reqCity = req.params.city;
+  const cityData = res.render("listing", { cityData });
+});
+
+router.get("/admin/:id", (req, res) => {
+  res.render("admin", { id });
+});
+
+router.post("/login", (req, res) => {
+  res.render("login");
+});
+
 router.post("/register", (req, res) => {
   res.render("register");
 });
 
-router.get("/listing/:city", (req, res) => {
-  res.render("listing");
-});
-
-router.get("/profile/:id", (req, res) => {
-  const id = req.id.params;
-  res.render("profile", { id });
-});
-
-router.post("/login", (req, res) => {
-  res.render("login", { id });
+router.post("/donate", (req, res) => {
+  res.render("profile");
 });
 
 router.post("/admin/:id", (req, res) => {
